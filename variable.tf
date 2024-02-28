dc1ff923fced01dab6e49385154879f64a5c4d51
Bustafab <ngafabius@gmail.com>
2024-02-28 11:51:29 -0800

    Adding some variables definition in the variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "value"
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance to provision"
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "The name of the EC2"
+  default = "EC2 provisioned by Terraform"
+}
